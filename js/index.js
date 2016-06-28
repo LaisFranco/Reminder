586a407fc36b6fc8bcfbbab4255a5a319c2b0822
Lais <lah_spfc2010@hotmail.com>
2016-06-28 19:55:24 -0300

    js
    inicio da criação do java script

---
+++ js/index.js
@@ -0,0 +1,61 @@
+var index = {
+    
+    init: function() {
+        index.setForm();
+        index.listReminder();
+    },
+    
+    //seter o formulario com o onsubmit
+    
+    setForm: function(){
+        var form = document.getElementById('form');
+        if(form){
+            form.onsubmit = function(){
+                index.saveReminder(form);
+                return false;
+            };
+        }
+    },
+    
+       //salva os dados
+    
+    saveReminder: function(form){
+        var reminder = {};
+        reminder.final = form.final.value;
+        reminder.data = form.data.value;
+        reminder.hora = form.hora.value;
+        
+        if(reminderDAO.save(reminder == reminderDAO.NEW){
+           tableController.addItem(reminder);
+        } else {
+            tableController.clearList();
+            index.listReminder();
+        }
+               
+            form.final.value = 
+            form.data.value =
+            form.hora.value = "";
+
+ },
+     
+     setTable: function(){
+        var table = document.getElementById('reminder');
+        tableController.setTable(table);
+    },
+        
+        listReminder: function () {
+            index.setTable();
+            var reminderList = reminderDAO.retrieve();
+            if (reminderList && reminderList.length){
+                tableController.addList(reminderList);
+            }
+        
+	}, 
+          
+reminderDAO.unserializeAndParse();
+index.init();
+        
+        
+            
+            
+           
